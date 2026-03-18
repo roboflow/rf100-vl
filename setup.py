f727f51bb9845f5858160bde56efeb579ba8bda6
Lee Clement <lee@roboflow.com>
2026-03-18 14:03:51 +0000

    bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="rf100vl",
-    version="1.1.0",
+    version="1.1.1",
     description="RF100-VL Dataset Interface",
     author="Roboflow, Inc.",
     author_email="peter@roboflow.com",
